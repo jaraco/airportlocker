0e408e8ed7a9e834ea6583729a289b65d894993a
Jason R. Coombs <jason.coombs@yougov.com>
2010-10-21 01:02:20 -0700

    Setup script now uses hgtools for versioning and package data discovery Loosened restriction on pmxtools

--- setup.py
+++ setup.py
@@ -1,28 +1,12 @@
 from setuptools import setup, find_packages
-from pmxtools import hgtools
 
-version = hgtools.get_tag_version() or 'devel'
-
-def strip_first_dir(pths):
-	def gen():
-		for p in pths:
-			yield p[p.find('/')+1:]
-	return list(gen())
-
-setup(
+setup_params = dict(
 	name='airportlocker',
-	version=version,
+	use_hg_version=True,
 	author="Eric Larson",
-	packages=find_packages('.'),	  
+	packages=find_packages(),	  
 	package_dir={'airportlocker': 'airportlocker'},
-	package_data={
-		'airportlocker' : (
-			strip_first_dir(hgtools.checked_in('airportlocker/view')) +
-			strip_first_dir(hgtools.checked_in('airportlocker/static')) +
-			strip_first_dir(hgtools.checked_in('airportlocker/data')) +
-			['etc/baseconf.yaml']
-		),
-	},
+	use_package_data=True,
 	data_files=[
 		('', ['unittest.yaml']),
 	],
@@ -34,8 +18,14 @@ setup(
 	install_requires=[
 		"eggmonster>=4.1,<4.2",
 		"fab>=2.3.3,<2.4",
-		"pmxtools>=0.15,<0.16",
+		"pmxtools>=0.15,<0.17dev",
 		"ottoman>=0.3.3,<0.4",
 		"simplejson",
-	]
+	],
+	setup_requires = [
+		'hgtools',
+	],
 )
+
+if __name__ == '__main__':
+	setup(**setup_params)
