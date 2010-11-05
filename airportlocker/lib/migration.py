c446e7dc600d1110123dc311c9550e3b1e1ac5ea
Jason R. Coombs <jason.coombs@yougov.com>
2010-11-05 08:20:23 -0700

    Adding migration module with code for migrating records from faststore

---
+++ airportlocker/lib/migration.py
@@ -0,0 +1,56 @@
+"""
+Routines to migrate from previous versions of airportlocker.
+"""
+
+import airportlocker
+json = airportlocker.json
+
+import logging
+log = logging.getLogger(__name__)
+
+def from_faststore():
+	"""
+	Migrate data from an airportlocker 0.7.x faststore
+	Requires that the fs_host, fs_port are still defined and that env.docset
+	is the same in the new mongodb as in the old faststore.
+	"""
+	import pkg_resources
+	pkg_resources.require('faststore')
+	from eggmonster import env
+	from faststore.client import FastStoreClient
+	has_faststore_config = 'fs_host' in dir(env) and 'fs_port' in dir(env)
+	if not has_faststore_config:
+		# if the faststore config has been removed, assume no migration is
+		#  necessary
+		return
+	log.info('FastStore configuration found - migrating records')
+	source = FastStoreClient(env.fs_host, env.fs_port)
+	dest = airportlocker.store[env.docset]
+	source_keys = set(source.keys(env.docset))
+	dest_keys = set(rec['_id'] for rec in dest.find())
+	missing = source_keys - dest_keys
+	if not missing:
+		log.info('No records in source not in dest - nothing to migrate.')
+		return
+	log.info('Migrating %d records', len(missing))
+	for key in missing:
+		if key.startswith('__view__'):
+			log.info('Skipping key %s', key)
+			continue
+		log.debug('Migrating %s', key)
+		try:
+			dest.insert(json.loads(source.retr(env.docset, key)))
+		except Exception:
+			log.error("Unhandled exception migrating %s", key)
+	log.info('Migration of %d records complete.', len(missing))
+
+def test_faststore_migration():
+	from eggmonster import env
+	env.fs_host = 'mongodb01.g.yougov.local'
+	env.fs_port = 10104
+	env.docset = 'luggage'
+	logging.basicConfig(level=logging.DEBUG)
+	import pymongo
+	airportlocker.store = pymongo.Connection()['airportlocker']
+	from_faststore()
+	
