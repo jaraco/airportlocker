b29f2efe388bc360bed8ae2ee60afaa2af8ca258
Jason R. Coombs <jaraco@jaraco.com>
2012-10-02 13:17:11 -0400

    Need to set up logging when running under velociraptor (since there's no eggmonster handler)

--- airportlocker/control/vr_launch.py
+++ airportlocker/control/vr_launch.py
@@ -1,15 +1,20 @@
 import os
 import importlib
+import logging
 
 import yaml
 
 import airportlocker
 from . import root
 
+def setup_logging():
+	logging.basicConfig(level=logging.INFO)
+
 def run():
 	with open(os.environ['APP_SETTINGS_YAML']) as config_stream:
 		airportlocker.config.update(yaml.load(config_stream))
 	airportlocker.config['airportlocker_port'] = int(os.environ['PORT'])
+	setup_logging()
 	importlib.import_module('airportlocker.etc.startup')
 	root.run()
 
