00118d9bc0356b7469cb1f4115b00a6209b26903
Jason R. Coombs <jaraco@jaraco.com>
2012-10-02 12:29:17 -0400

    Added pavement file to omit airportlocker dir

---
+++ pavement.py
@@ -0,0 +1,13 @@
+import shutil
+
+from paver.easy import task
+
+@task
+def build():
+	"""
+	Velociraptor build step
+	"""
+	# when velociraptor runs pip, it installs '.', but the package
+	# 'airportlocker' is still here, and causes warnings on startup, so
+	# remove it.
+	shutil.rmtree('airportlocker')
