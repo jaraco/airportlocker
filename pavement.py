33ac822c0e729c0390aeb004fcfe4bb24238e68d
Jason R. Coombs <jaraco@jaraco.com>
2012-10-16 07:29:47 -0400

    Spaces for indent

--- pavement.py
+++ pavement.py
@@ -4,10 +4,10 @@ from paver.easy import task
 
 @task
 def build():
-	"""
-	Velociraptor build step
-	"""
-	# when velociraptor runs pip, it installs '.', but the package
-	# 'airportlocker' is still here, and causes warnings on startup, so
-	# remove it.
-	shutil.rmtree('airportlocker')
+    """
+    Velociraptor build step
+    """
+    # when velociraptor runs pip, it installs '.', but the package
+    # 'airportlocker' is still here, and causes warnings on startup, so
+    # remove it.
+    shutil.rmtree('airportlocker')
