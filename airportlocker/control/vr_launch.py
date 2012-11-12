8ee11c075e2406e510be1228d5607c20dfd35e01
fernando.gutierrez <none@none>
2012-11-12 16:43:07 -0500

    Don't fail if there is no APP_SETTINGS_YAML

--- airportlocker/control/vr_launch.py
+++ airportlocker/control/vr_launch.py
@@ -11,8 +11,9 @@ def setup_logging():
 	logging.basicConfig(level=logging.INFO)
 
 def run():
-	with open(os.environ['APP_SETTINGS_YAML']) as config_stream:
-		airportlocker.config.update(yaml.load(config_stream))
+	if 'APP_SETTINGS_YAML' in os.environ:
+		with open(os.environ['APP_SETTINGS_YAML']) as config_stream:
+			airportlocker.config.update(yaml.load(config_stream))
 	airportlocker.config['airportlocker_port'] = int(os.environ['PORT'])
 	setup_logging()
 	importlib.import_module('airportlocker.etc.startup')
