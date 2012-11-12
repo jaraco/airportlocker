dcd1d61414852d0d5b4f4ec9ef524a7c5cc1cab7
fernando.gutierrez <none@none>
2012-11-12 18:23:57 -0500

    Added a fallback for VR launch script so we can use a --config= in the gryphon Procfile to customize default config

--- airportlocker/control/vr_launch.py
+++ airportlocker/control/vr_launch.py
@@ -1,11 +1,13 @@
-import os
+import getopt
 import importlib
 import logging
+import os
+import sys
 
 import yaml
 
 import airportlocker
-from . import root
+from airportlocker.control import root
 
 def setup_logging():
 	logging.basicConfig(level=logging.INFO)
@@ -14,6 +16,18 @@ def run():
 	if 'APP_SETTINGS_YAML' in os.environ:
 		with open(os.environ['APP_SETTINGS_YAML']) as config_stream:
 			airportlocker.config.update(yaml.load(config_stream))
+	else:
+		try:
+			opts, args = getopt.getopt(sys.argv[1:],
+						   'c', ['config='])
+		except getopt.GetoptError as err:
+			print srt(err)
+			sys.exit(2)
+		for o, a in opts:
+			if o in ('-c', '--config'):
+				with open(a) as config_stream:
+					airportlocker.config.update(
+						yaml.load(config_stream))
 	airportlocker.config['airportlocker_port'] = int(os.environ['PORT'])
 	setup_logging()
 	importlib.import_module('airportlocker.etc.startup')
