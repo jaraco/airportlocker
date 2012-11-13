0a16ac5859bb97ba8008a58e666b85165f7a5014
fernando.gutierrez <none@none>
2012-11-13 18:50:45 -0500

    Updated urls for the new OPTIONS available

--- airportlocker/control/urls.py
+++ airportlocker/control/urls.py
@@ -13,14 +13,16 @@ api = FabDispatcher()
 api.add_route('read', 'static/*', ReadResource(), method='GET')
 api.add_route('head', 'static/*', ReadResource(), method='HEAD')
 api.add_route('view', 'view/:id', ViewResource(), method='GET')
+api.add_route('view-options', 'view/:id', ViewResource(), method='OPTIONS')
 # overloaded POST for browsers
 # TODO: make _method tool
 api.add_route('post_delete', 'edit/:id', DeleteResource(), method='POST')
 api.add_route('post_update', 'edit/:id', UpdateResource(), method='PUT')
 api.add_route('delete', 'edit/:id', DeleteResource(), method='DELETE')
 api.add_route('index', '', ListResources(), method='GET')
+api.add_route('index-options', '', ListResources(), method='OPTIONS')
 api.add_route('create', '', CreateResource(), method='POST')
-api.add_route('options', '', CreateResource(), method='OPTIONS')
+api.add_route('create-options', '', CreateResource(), method='OPTIONS')
 
 dev = FabDispatcher()
 dev.add_route('index', '_dev', BasicUpload(), method='GET')
