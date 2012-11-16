ae94191b834e30d2932ffc900c80d6f29b29aee6
fernando.gutierrez <none@none>
2012-11-16 17:42:44 -0500

    Fixed prefix handling so using _prefix in Gryphon Admin now stores at /static/SurveyName/FileName.ext

--- airportlocker/control/main.py
+++ airportlocker/control/main.py
@@ -11,151 +11,157 @@ from airportlocker.control.base import Resource, HtmlResource, post
 from airportlocker.lib.storage import NotFoundError
 
 def success(value):
-	return json.dumps({'status': 'success', 'value': value})
+    return json.dumps({'status': 'success', 'value': value})
 
 def failure(message):
-	return json.dumps({'status': 'failure', 'reason': message})
+    return json.dumps({'status': 'failure', 'reason': message})
 
 def items(field_storage):
-	"""
-	Like dict.items, but for cgi.FieldStorage
-	"""
-	for key in field_storage.keys():
-		yield key, field_storage.getvalue(key)
+    """
+    Like dict.items, but for cgi.FieldStorage
+    """
+    for key in field_storage.keys():
+        yield key, field_storage.getvalue(key)
 
 class BasicUpload(HtmlResource):
-	template = fab.template('base.tmpl')
-	body = fab.template('basicform.tmpl')
-	def GET(self, page, *args, **kw):
-		cherrypy.response.headers['Content-Type'] = 'application/xhtml+xml'
-		page.args = kw
+    template = fab.template('base.tmpl')
+    body = fab.template('basicform.tmpl')
+    def GET(self, page, *args, **kw):
+        cherrypy.response.headers['Content-Type'] = 'application/xhtml+xml'
+        page.args = kw
 
 
 class ListResources(Resource, airportlocker.storage_class):
-	def GET(self, page, **kw):
-		cherrypy.response.headers['Cache-Control'] = 'no-cache'
-		# traditionally, q must be __all to query all. Now that's the default
-		#  behavior if no kw is passed... but support it for compatibility.
-		kw.pop('q', None)
-		if kw:
-			res = self._query(**kw)
-		else:
-			res = self._list()
-		return json.dumps(res)
+    def GET(self, page, **kw):
+        cherrypy.response.headers['Cache-Control'] = 'no-cache'
+        # traditionally, q must be __all to query all. Now that's the default
+        #  behavior if no kw is passed... but support it for compatibility.
+        kw.pop('q', None)
+        if kw:
+            res = self._query(**kw)
+        else:
+            res = self._list()
+        return json.dumps(res)
 
-	def _list(self):
-		def add_url(row):
-			row['url'] = '/static/%(_id)s' % row
-			row['_id'] = str(row['_id'])
-			return row
-		return map(add_url, self.find())
+    def _list(self):
+        def add_url(row):
+            row['url'] = '/static/%(_id)s' % row
+            row['_id'] = str(row['_id'])
+            return row
+        return map(add_url, self.find())
 
-	def _query(self, **kw):
-		"""
-		Return all records that match the query.
-		"""
-		return list(self.find(kw))
+    def _query(self, **kw):
+        """
+        Return all records that match the query.
+        """
+        return list(self.find(kw))
 
 class ViewResource(Resource, airportlocker.storage_class):
-	def GET(self, page, id):
-		results = self.find_one(self.by_id(id))
-		if not results:
-			raise cherrypy.NotFound()
-		return json.dumps(results)
+    def GET(self, page, id):
+        results = self.find_one(self.by_id(id))
+        if not results:
+            raise cherrypy.NotFound()
+        return json.dumps(results)
 
 class ReadResource(Resource, airportlocker.storage_class):
-	def GET(self, page, *args, **kw):
-		if not args:
-			raise cherrypy.NotFound()
-		path = '/'.join(args)
-		cherrypy.response.headers['Connection'] = 'close'
-		return self.return_file(path)
+    def GET(self, page, *args, **kw):
+        if not args:
+            raise cherrypy.NotFound()
+        path = '/'.join(args)
+        cherrypy.response.headers['Connection'] = 'close'
+        return self.return_file(path)
 
-	def HEAD(self, page, *args, **kw):
-		path = '/'.join(args)
-		cherrypy.response.headers['Connection'] = 'close'
-		return self.head_file(path)
+    def HEAD(self, page, *args, **kw):
+        path = '/'.join(args)
+        cherrypy.response.headers['Connection'] = 'close'
+        return self.head_file(path)
 
-	def return_file(self, path):
-		try:
-			resource, ct = self.get_resource(path)
-		except NotFoundError:
-			raise cherrypy.NotFound()
-		cherrypy.response.headers.update({
-			'Content-Type': ct or 'text/plain',
-		})
-		return resource
+    def return_file(self, path):
+        try:
+            resource, ct = self.get_resource(path)
+        except NotFoundError:
+            raise cherrypy.NotFound()
+        cherrypy.response.headers.update({
+            'Content-Type': ct or 'text/plain',
+        })
+        return resource
 
-	def head_file(self, path):
-		try:
-			resource, ct = self.get_resource(path)
-		except NotFoundError:
-			raise cherrypy.NotFound()
-		size = sum([len(l) for l in resource])
-		cherrypy.response.headers.update({
-			'Content-Type': ct or 'text/plain',
-			'Content-Length': size,
-		})
-		return
+    def head_file(self, path):
+        try:
+            resource, ct = self.get_resource(path)
+        except NotFoundError:
+            raise cherrypy.NotFound()
+        size = sum([len(l) for l in resource])
+        cherrypy.response.headers.update({
+            'Content-Type': ct or 'text/plain',
+            'Content-Length': size,
+        })
+        return
 
 class CreateResource(Resource, airportlocker.storage_class):
-	'''
-	Save the file and make sure the filename is as close as possible to the
-	original while still being unique.
-	'''
+    '''
+    Save the file and make sure the filename is as close as possible to the
+    original while still being unique.
+    '''
 
-	@post
-	def POST(self, page, fields):
-		'''Save the file to storage'''
-		fields_valid = '_new' in fields and '_lockerfile' in fields
-		if not fields_valid:
-			return failure('A "_new" and "_lockerfile" are required to '
-				'create a new document.')
+    @post
+    def POST(self, page, fields):
+        '''Save the file to storage'''
+        fields_valid = '_new' in fields and '_lockerfile' in fields
+        if not fields_valid:
+            return failure('A "_new" and "_lockerfile" are required to '
+                'create a new document.')
 
-		# cast fields to a dict here because CGIFieldStorage doesn't have
-		#  a pop attribute.
-		prefix = dict(fields).pop('_prefix', '')
-		stream = fields['_lockerfile'].file
-		content_type = fields['_lockerfile'].type
+        # cast fields to a dict here because CGIFieldStorage doesn't have
+        # a pop attribute. Also we can't pass FieldStorage to posixpath
+        # later so use it's value if it is there.
+        prefix = fields.get('_prefix', None)
+        if prefix is not None:
+            prefix = prefix.value
 
-		# use override name field if supplied, else use source filename
-		name = (fields['name'].value
-			if 'name' in fields else fields['_lockerfile'].filename)
-		# but always trust the original filename for the extension
-		name = self._ensure_extension(name, fields['_lockerfile'].filename)
+        stream = fields['_lockerfile'].file
+        content_type = fields['_lockerfile'].type
 
-		filepath = posixpath.join(prefix, name)
+        # use override name field if supplied, else use source filename
+        name = (fields['name'].value
+            if 'name' in fields else fields['_lockerfile'].filename)
+        # but always trust the original filename for the extension
+        name = self._ensure_extension(name, fields['_lockerfile'].filename)
 
-		# metadata are all fields that don't begin with '_'
-		meta = dict(
-			(k, v) for k, v in items(fields)
-			if not k.startswith('_')
-		)
-		return success(self.save(stream, filepath, content_type, meta))
+        filepath = posixpath.join(prefix, name)
+
+        # metadata are all fields that don't begin with '_'
+        meta = dict(
+            (k, v) for k, v in items(fields)
+            if not k.startswith('_')
+        )
+        oid = self.save(stream, filepath, content_type, meta)
+
+        return success(oid)
 
 class UpdateResource(Resource, airportlocker.storage_class):
 
-	@post
-	def PUT(self, page, fields, id):
-		file_ob = fields.get('_lockerfile', None)
-		params = dict(
-			stream = file_ob.file,
-			content_type = file_ob.type,
-		) if file_ob else dict()
+    @post
+    def PUT(self, page, fields, id):
+        file_ob = fields.get('_lockerfile', None)
+        params = dict(
+            stream = file_ob.file,
+            content_type = file_ob.type,
+        ) if file_ob else dict()
 
-		# metadata are all fields that don't begin with '_'
-		meta = dict(
-			(k, v) for k, v in items(fields)
-			if not k.startswith('_')
-		)
+        # metadata are all fields that don't begin with '_'
+        meta = dict(
+            (k, v) for k, v in items(fields)
+            if not k.startswith('_')
+        )
 
-		try:
-			new_doc = self.update(id, meta=meta, **params)
-		except NotFoundError:
-			raise cherrypy.NotFound()
+        try:
+            new_doc = self.update(id, meta=meta, **params)
+        except NotFoundError:
+            raise cherrypy.NotFound()
 
-		return success({'updated': json.dumps(new_doc)})
+        return success({'updated': json.dumps(new_doc)})
 
 class DeleteResource(Resource, airportlocker.storage_class):
-	def DELETE(self, page, id):
-		return success({'deleted': self.delete(id)})
+    def DELETE(self, page, id):
+        return success({'deleted': self.delete(id)})
