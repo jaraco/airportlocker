7e0155fde8b982541cda329c113351487349be35
fernando.gutierrez <none@none>
2012-11-30 20:05:37 -0500

    Added survey name param to new_api so we support not namespaced resource and resources.url helper calls.

--- airportlocker/lib/client.py
+++ airportlocker/lib/client.py
@@ -10,85 +10,93 @@ pjoin = posixpath.join
 
 class AirportLockerClient(object):
 
-	def __init__(self, url, h=None):
-		self.base = url
-		self.h = h or httplib2.Http('.ap_cache')
-		self._api = {
-			'query': '',
-			'create': '',
-			'update': '/edit',
-			'read': '/static',
-			'view': '/view',
-			'delete': '/edit',
-		}
+    def __init__(self, url, h=None):
+        self.base = url
+        self.h = h or httplib2.Http('.ap_cache')
+        self._api = {
+            'query': '',
+            'create': '',
+            'update': '/edit',
+            'read': '/static',
+            'view': '/view',
+            'delete': '/edit',
+        }
 
-	def api(self, action, tail=None, prefix=None, use_host=True):
-		tail = tail or ''
-		prefix = prefix or ''
-		base = self.base
-		if not use_host:
-			base = ''
-		result = urlparse.urljoin(base, pjoin(self._api[action], prefix, tail))
-		return result
+    def api(self, action, tail=None, prefix=None, use_host=True):
+        tail = tail or ''
+        prefix = prefix or ''
+        base = self.base
+        if not use_host:
+            base = ''
+        result = urlparse.urljoin(base, pjoin(self._api[action], prefix, tail))
+        return result
 
-	def new_api(self, filename):
-		""" New API call, instead of constructing a URL it goes to
-		airportlocker, searches for the file and gets the cached md5 url.
-		"""
-		url = urlparse.urljoin(self.base, self.api('query')) + '?' + \
+    def new_api(self, filename, survey_name):
+        """ New API call, instead of constructing a URL it goes to
+        airportlocker, searches for the file and gets the cached md5 url.
+        """
+        if not filename.startswith('/'):
+            filename = '/' + filename
+
+        url = urlparse.urljoin(self.base, self.api('query')) + '?' + \
               urllib.urlencode({'filename': filename})
-		response, content = self.h.request(url, method='GET')
-		filejson = json.loads(content)
-		if len(filejson):
-			return urlparse.urljoin(self.base, filejson[0]['cached_url'])
-		return 'invalid file'
+        response, content = self.h.request(url, method='GET')
+        filejson = json.loads(content)
+        if not len(filejson) and not filename.startswith('/' + survey_name):
+            # Try to find the file with the survey_name as prefix
+            return self.new_api('/' + survey_name + filename, survey_name)
+
+        if len(filejson):
+            return urlparse.urljoin(self.base, filejson[0]['cached_url'])
+
+        return '/404/'
 
 
-	def create(self, fn, fields=None):
-		fields = fields or {}
-		fields.update({'_new': 'True'})
-		data = MultiPart(fn, fields)
-		res, c = self.h.request(self.api('create'),
-								method='POST',
-								body=data.body,
-								headers=data.headers)
-		if res.status < 300:
-			response = json.loads(c)
-		else:
-			res['content'] = c
-			return res
-		return response
+    def create(self, fn, fields=None):
+        fields = fields or {}
+        fields.update({'_new': 'True'})
+        data = MultiPart(fn, fields)
+        res, c = self.h.request(self.api('create'),
+                                method='POST',
+                                body=data.body,
+                                headers=data.headers)
+        if res.status < 300:
+            response = json.loads(c)
+        else:
+            res['content'] = c
+            return res
+        return response
 
-	def update(self, id, fn, fields=None):
-		fields = fields or {}
-		data = MultiPart(fn, fields)
-		res, c = self.h.request(self.api('update', id),
-								method='PUT',
-								body=data.body,
-								headers=data.headers)
-		response = json.loads(c)
-		return response
+    def update(self, id, fn, fields=None):
+        fields = fields or {}
+        data = MultiPart(fn, fields)
+        res, c = self.h.request(self.api('update', id),
+                                method='PUT',
+                                body=data.body,
+                                headers=data.headers)
+        response = json.loads(c)
+        return response
 
-	def view(self, id):
-		res, c = self.h.request(self.api('view', id))
-		return json.loads(c)
+    def view(self, id):
+        res, c = self.h.request(self.api('view', id))
+        return json.loads(c)
 
-	def read(self, path, prefix=''):
-		res, c = self.h.request(self.api('read', path))
-		return c
+    def read(self, path, prefix=''):
+        res, c = self.h.request(self.api('read', path))
+        return c
 
-	def delete(self, id):
-		res, c = self.h.request(self.api('delete', id), method='DELETE')
-		response = json.loads(c)
-		return response
+    def delete(self, id):
+        res, c = self.h.request(self.api('delete', id), method='DELETE')
+        response = json.loads(c)
+        return response
 
-	def exists(self, id, prefix=None):
-		prefix = prefix or ''
-		res, c = self.h.request(self.api('read', id, prefix=prefix),
+    def exists(self, id, prefix=None):
+        prefix = prefix or ''
+        res, c = self.h.request(self.api('read', id, prefix=prefix),
             method='HEAD')
-		return res['status'].startswith('20')
+        return res['status'].startswith('20')
 
-	def query(self, qs):
-		qs = '?q=%s' % qs
-		res, c = self.h.request(self.api('query', qs))
-		return json.loads(c)
+    def query(self, qs):
+        qs = '?q=%s' % qs
+        res, c = self.h.request(self.api('query', qs))
+        return json.loads(c)
