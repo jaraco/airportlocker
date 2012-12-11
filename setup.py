67cb59f7e71fb7fce74c05b8f437627f516a4f37
fernando.gutierrez <none@none>
2012-12-11 11:00:57 -0500

    Updating requirements to include zencoder (unofficial) python lib. And adding me to the authors \o/

--- setup.py
+++ setup.py
@@ -6,44 +6,45 @@ py25reqs = ['simplejson'] if sys.version_info < (2, 6) else []
 py26reqs = ['importlib'] if sys.version_info < (2, 7) else []
 
 setup_params = dict(
-	name='airportlocker',
-	use_hg_version=dict(increment='0.0.1'),
-	author="Eric Larson/Jason R. Coombs",
-	author_email="Eric.Larson@YouGov.com",
-	url="http://dev.yougov.com/",
-	packages=setuptools.find_packages(),
-	include_package_data=True,
-	entry_points={
-		'eggmonster.applications': [
-			'main = airportlocker.control.em_launch:run',
-		],
-	},
-	install_requires=[
-		'pyYAML',
-		"yg.process>=1.0.2,<2.0dev",
-		'jaraco.util',
-		'path.py',
-	] + py25reqs + py26reqs,
-	extras_require=dict(
-		# note, if you change the server requirements, you must also update
-		#  the requirements.txt (because pip doesn't support extras)
-		server=[
-			"fab>=2.4,<4.0dev",
-			"pymongo>=2.3",
-		],
-		client=[
-			"httplib2",
-		],
-	),
-	setup_requires = [
-		'hgtools',
-		'pytest-runner',
-	],
-	tests_require = [
-		'pytest',
-		'mock',
-	],
+    name='airportlocker',
+    use_hg_version=dict(increment='0.0.1'),
+    author="Eric Larson/Jason R. Coombs/Fernando Gutierrez",
+    author_email="Eric.Larson@YouGov.com",
+    url="http://dev.yougov.com/",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={
+        'eggmonster.applications': [
+            'main = airportlocker.control.em_launch:run',
+        ],
+    },
+    install_requires=[
+        'pyYAML',
+        "yg.process>=1.0.2,<2.0dev",
+        'jaraco.util',
+        'path.py',
+    ] + py25reqs + py26reqs,
+    extras_require=dict(
+        # note, if you change the server requirements, you must also update
+        #  the requirements.txt (because pip doesn't support extras)
+        server=[
+            "fab>=2.4,<4.0dev",
+            "pymongo>=2.3",
+            "zencoder"
+        ],
+        client=[
+            "httplib2",
+        ],
+    ),
+    setup_requires = [
+        'hgtools',
+        'pytest-runner',
+    ],
+    tests_require = [
+        'pytest',
+        'mock',
+    ],
 )
 
 if __name__ == '__main__':
-	setuptools.setup(**setup_params)
+    setuptools.setup(**setup_params)
