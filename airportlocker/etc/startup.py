1d8b1fe9ff7c5a27a7c2ad037a905b3b56520e59
Jason R. Coombs <jaraco@jaraco.com>
2012-12-20 21:38:25 -0500

    Need to specify default_db_name in case the config doesn't

--- airportlocker/etc/startup.py
+++ airportlocker/etc/startup.py
@@ -63,6 +63,7 @@ if airportlocker.config.get('filestore', None):
 airportlocker.storage_class = _get_storage_class()
 airportlocker.storage_class.startup()
 _build_uri_from_legacy_config()
-airportlocker.store = yg.mongodb.connect_db(airportlocker.config.storage_uri)
+airportlocker.store = yg.mongodb.connect_db(airportlocker.config.storage_uri,
+    default_db_name='airportlocker')
 
 _do_migration()
