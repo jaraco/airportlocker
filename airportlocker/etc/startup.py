6d48e11e3f9d6b3a4d958aeffcde54704388aa15
Jason R. Coombs <jaraco@jaraco.com>
2013-01-09 20:51:24 -0500

    Removed legacy config support

--- airportlocker/etc/startup.py
+++ airportlocker/etc/startup.py
@@ -25,27 +25,6 @@ def _get_storage_class():
     factory = eval(factory_exp, module.__dict__)
     return factory
 
-def _build_uri_from_legacy_config():
-    """
-    Prior to airportlocker 1.8, config could be specified using these keys:
-      - mongo_host
-      - mongo_port
-      - mongo_db_name
-
-    If any of those parameters are present, construct the storage_uri from
-    those parameters (and their former defaults).
-    """
-    legacy_keys = 'mongo_host', 'mongo_port', 'mongo_db_name'
-    if not any(key in airportlocker.config for key in legacy_keys):
-        return
-    host = airportlocker.config.get('mongo_host', 'localhost')
-    port = airportlocker.config.get('mongo_port', 27017)
-    if port != 27017:
-        host += ':{port}'.format(port=port)
-    db_name = airportlocker.config.get('mongo_db_name', 'airportlocker')
-    airportlocker.config['storage_uri'] = 'mongodb://{host}/{db_name}'.format(
-        host=host, db_name=db_name)
-
 logging.getLogger('airportlocker').info('starting')
 
 # ensure filestore is created as a path.path object.
@@ -55,6 +34,5 @@ if airportlocker.config.get('filestore', None):
 
 airportlocker.storage_class = _get_storage_class()
 airportlocker.storage_class.startup()
-_build_uri_from_legacy_config()
 airportlocker.store = yg.mongodb.connect_db(airportlocker.config.storage_uri,
     default_db_name='airportlocker')
