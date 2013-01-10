13b197be7ec047c91ffe2c2c694d20c9f799579a
Jason R. Coombs <jaraco@jaraco.com>
2013-01-10 13:59:46 -0500

    Moved zencoder config to default config

--- airportlocker/__init__.py
+++ airportlocker/__init__.py
@@ -26,6 +26,26 @@ config = yg.launch.config.ConfigDict(
     log_host = "vesyslogd",
     log_port = 13000,
     log_facility = "airportlocker",
+
+    # Integration only zencoder api key. Use a commercial key in production
+    #  to support longer videos.
+    zencoder_api_key = '06f15509882cc8afb0565af79a9206e1',
+    zencoder_outputs = [
+        {
+            'label': 'HTML5/Flash/MP4/H.264', 'h264_profile': 'high',
+            'extension': 'mp4', 'suffix': 'html5_mp4',
+        },
+        {
+            'label': 'HTML5/WebM', 'extension': 'webm',
+            'suffix': 'html5_webm',
+        },
+        {'label': 'HTML5/Ogg', 'extension': 'ogg', 'suffix': 'html5_ogg',},
+        {
+            'label': 'Mobile/3GP/MPEG4 non-smartphones', 'size': '320x240',
+            'extension': '3gp', 'suffix': 'mov_3gp',
+        },
+    ],
+
 )
 
 _default_config = yg.launch.config.ConfigDict(config)
