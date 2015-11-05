a1fac22384c3438c3a3b35474db3edbe3aa778d7
Jason R. Coombs <jaraco@jaraco.com>
2015-11-05 07:04:01 -0500

    Modernize runner

--- test/runner.py
+++ test/runner.py
@@ -1,12 +1,15 @@
-from optparse import OptionParser
+import argparse
 import timeit
 
-if __name__ == '__main__':
-    usage = 'usage: %prog {airportlocker url}'
-    parser = OptionParser()
-    (options, args) = parser.parse_args()
-    if args < 1:
-        parser.error('I need the url!')
-    stmt = "call(['python', 'exerciser.py', '%s'])" % args[0]
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url')
+    args = parser.parse_args()
+    stmt = "call(['python', 'exerciser.py', '%s'])" % args.url
     timer = timeit.Timer(stmt, 'from subprocess import call')
-    print timer.timeit(number=10)
+    print(timer.timeit(number=10))
+
+
+if __name__ == '__main__':
+    run()
