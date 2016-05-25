e09caf73fb5d78244db02f8b3565a29be6a2b3b4
Jason R. Coombs <jaraco@jaraco.com>
2016-05-25 07:21:09 -0400

    Update uptest to use portend and requests for cleaner startup and Python 3 support

--- uptests/web/responding.py
+++ uptests/web/responding.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python
 
-import urllib2
 import argparse
-import json
-import time
-import itertools
+import requests
+import portend
 
 parser = argparse.ArgumentParser()
 parser.add_argument('host')
 parser.add_argument('port', type=int)
 args = parser.parse_args()
+portend.occupied(args.host, args.port, timeout=10)
 
 root = 'http://{host}:{port}/?surveyName=uptest'.format(**vars(args))
-for try_ in itertools.count(1):
-    try:
-        items = json.load(urllib2.urlopen(root))
-        break
-    except urllib2.URLError as exc:
-        if 'refused' not in unicode(exc).lower() or try_ >= 3:
-            raise
-        time.sleep(3)
+status = requests.get(root).json()
+assert status, "Empty status received"
